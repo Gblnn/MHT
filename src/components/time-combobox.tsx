6cc6abca8217b5129d621f65f6caa831670198d9
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-02-25 22:21:21 +0400

    Update time-combobox.tsx

--- src/components/time-combobox.tsx
+++ src/components/time-combobox.tsx
@@ -21,19 +21,24 @@ import {
           style={{
             background: "#1a1a1a",
             border: "1px solid #4a4a4a",
-            fontSize: "1rem",
-            flex:1.5
+            fontSize: "1.25rem",
+            flex:1.5,
+            
           }}
         >
           <SelectValue placeholder={props.placeholder} />
         </SelectTrigger>
-        <SelectContent style={{ background: "#1a1a1a", color: "white", height:"15ch" }}>
-          <SelectItem value="6:00">6:00</SelectItem>
-          <SelectItem value="6:30">6:30</SelectItem>
-          <SelectItem value="7:00">7:00</SelectItem>
-          <SelectItem value="7:30">7:30</SelectItem>
-          <SelectItem value="8:00">8:00</SelectItem>
-          <SelectItem value="8:30">8:30</SelectItem>
+        <SelectContent style={{ background: "#1a1a1a", color: "white", height:"10rem", fontSize:"1.25rem" }}>
+          <SelectItem style={{fontSize:"1.25rem"}} value="5:30">5:30</SelectItem>
+          <SelectItem style={{fontSize:"1.25rem"}} value="6:00">6:00</SelectItem>
+          <SelectItem style={{fontSize:"1.25rem"}} value="6:30">6:30</SelectItem>
+          <SelectItem style={{fontSize:"1.25rem"}} value="7:00">7:00</SelectItem>
+          <SelectItem style={{fontSize:"1.25rem"}} value="7:30">7:30</SelectItem>
+          <SelectItem style={{fontSize:"1.25rem"}} value="8:00">8:00</SelectItem>
+          <SelectItem style={{fontSize:"1.25rem"}} value="8:30">8:30</SelectItem>
+          <SelectItem style={{fontSize:"1.25rem"}} value="8:30">8:30</SelectItem>
+          <SelectItem style={{fontSize:"1.25rem"}} value="9:00">9:00</SelectItem>
+          <SelectItem style={{fontSize:"1.25rem"}} value="9:30">9:30</SelectItem>
           
           
           
