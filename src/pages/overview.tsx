647f0d6f6457001d47a9e91720215a3ca3bcde12
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-03-02 15:50:22 +0400

    Update overview.tsx

--- src/pages/overview.tsx
+++ src/pages/overview.tsx
@@ -130,7 +130,7 @@ export default function Supervision() {
                 
               />
             ))} */}
-            <h1 style={{fontWeight:600, fontSize:"1.25rem", padding:"0.05rem", background:"var(--clr-opacity)", borderRadius:"1rem", paddingLeft:"1rem", paddingRight:"1rem", opacity:0.75}}>{moment().format("LL")}</h1>
+            <h1 style={{fontWeight:600, fontSize:"1.25rem", padding:"0.05rem", background:"var(--clr-opacity)", borderRadius:"1rem", paddingLeft:"1rem", paddingRight:"1rem"}}>{moment().format("LL")}</h1>
             
               <table style={{tableLayout:"fixed", width:"100%", textAlign:"center"}}>
                 <thead>
@@ -170,7 +170,7 @@ export default function Supervision() {
               {records.length<1?
               <div style={{ width:"100%",height:"90%", background:"", display:"flex", justifyContent:"center",alignItems:"center",fontSize:"1rem", position:"absolute"}}>
                 {loading?<LoadingOutlined style={{fontSize:"2rem", color:"crimson"}}/>
-                :<p style={{background:"var(--clr-opacity)", padding:"0.5rem", borderRadius:"1rem", paddingLeft:"1rem", paddingRight:"1rem",display:"flex", gap:"0.4rem", alignItems:"center", opacity:0.75 }}><Package width="1.1rem"/> Record Empty</p>}
+                :<p style={{background:"var(--clr-opacity)", padding:"0.5rem", borderRadius:"1rem", paddingLeft:"1rem", paddingRight:"1rem",display:"flex", gap:"0.4rem", alignItems:"center", opacity:0.75, userSelect:"none" }}><Package width="1.1rem"/> Record Empty</p>}
               
               </div>
               :null
