456a7ca30cd97ba51099504df35563e5ec3ee78f
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-03-04 15:56:24 +0400

    update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header(){
 
     
     return(
-        <div className="header" style={{}}>
+        <div className="header" style={{zIndex:10}}>
             <div className="header-title" style={{paddingRight:"1.5rem", paddingLeft:"1.5rem", display:"flex", alignItems:"center", gap:"0.25rem"}}>
                 <Globe2/>
                 <h1 style={{fontSize:"1.75rem", fontWeight:800}}>MHT</h1>
