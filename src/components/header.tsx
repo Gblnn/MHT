cf78b30b4f2ed1d0ca81a5dbc8c8763bd2579c89
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-03-07 12:56:21 +0400

    update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,3 @@
-import { Globe2 } from "lucide-react"
 import UserButton from "./user-button"
 
 
@@ -8,7 +7,8 @@ export default function Header(){
     return(
         <div className="header" style={{zIndex:10}}>
             <div className="header-title" style={{paddingRight:"1.5rem", paddingLeft:"1.5rem", display:"flex", alignItems:"center", gap:"0.25rem",color:"var(--clr-accent)"}}>
-                <Globe2/>
+                {/* <Globe2/> */}
+                <img src="/logo.png" style={{width:"2.25rem"}}/>
                 <h1 style={{fontSize:"1.75rem", fontWeight:800}}>MHT</h1>
             </div>
 
