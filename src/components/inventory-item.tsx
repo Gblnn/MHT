3ff13e55c38e5fe0bb79c1ee767589d8d9277cc6
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-03-19 00:43:02 +0400

    update

--- src/components/inventory-item.tsx
+++ src/components/inventory-item.tsx
@@ -7,10 +7,13 @@ interface Props {
 
 export default function InventoryItem(props:Props){
     return(
-        <div className="inventory-item" style={{background:"rgba(220 220 220)", width:"25ch", height:"30ch", margin:"1rem", borderRadius:"1rem"}}>
+        <div className="inventory-item" style={{background:"rgba(220 220 220)", width:"28ch", height:"32ch", margin:"1rem", borderRadius:"1rem"}}>
             <div style={{ width:"100%", height:"100%", padding:"0.75rem"}}>
-                <div id="picture-grid" style={{width:"100%", height:"75%", background:"rgba(100 100 100/ 25%)", borderRadius:"0.5rem"}}>
-                    <h1 style={{background:"var(--clr-accent)", borderRadius:"0.5rem", width:"fit-content", color:"white", padding:"0.25rem", paddingRight:"0.5rem", paddingLeft:"0.5rem", margin:"0.5rem", position:"absolute"}}>HDPE</h1>
+                <div id="picture-grid" style={{width:"100%", height:"75%", background:"rgba(100 100 100/ 25%)", borderRadius:"0.5rem", display:"flex"}}>
+                    {/* <h1 style={{background:"var(--clr-accent)", borderRadius:"0.5rem", width:"fit-content", color:"white", padding:"0.25rem", paddingRight:"0.5rem", paddingLeft:"0.5rem", margin:"0.5rem", position:"relative", zIndex:1}}>HDPE</h1> */}
+
+                    {/* <h1 style={{background:"var(--clr-accent)", borderRadius:"0.5rem", width:"fit-content", color:"white", padding:"0.25rem", paddingRight:"0.5rem", paddingLeft:"0.5rem", margin:"0.5rem", right:0}}>HDPE</h1> */}
+                    
                     <img src={props.image} style={{objectFit:"cover", width:"100%", height:"100%", borderRadius:"0.5rem"}}/>
                 </div>
 
