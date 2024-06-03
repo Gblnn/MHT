8514e6a053ad74ad2fdcd27fdaa656f8544993f8
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-06-03 10:30:12 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import './styles/utils.css'
 import '../app/globals.css'
 import { ConfigProvider, theme } from 'antd'
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ConfigProvider theme={{algorithm: theme.darkAlgorithm}}>
